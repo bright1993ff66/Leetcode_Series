136774e02805a8c8c2d938fcc0b2b3309cf50583
Bright Chang <75425626@qq.com>
2020-12-18 16:10:21 +0800

    add sum of left leaves

---
+++ DataStructures_Algos/Tree/404_Sum_of_Left_leaves.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        if not root: return 0
+        if self.isleaf(root.left):
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+
+    def isleaf(self, node):
+        if not node: return False
+        return not node.left and not node.right
