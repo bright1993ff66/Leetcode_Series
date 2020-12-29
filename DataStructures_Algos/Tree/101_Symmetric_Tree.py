e7ccd69223d07357c5642ecb36a1434dcc17a51c
bright1993 <75425626@qq.com>
2020-12-29 16:45:06 +0800

    edit codes for 101 symmetric tree

--- DataStructures_Algos/Tree/101_Symmetric_Tree.py
+++ DataStructures_Algos/Tree/101_Symmetric_Tree.py
@@ -11,23 +11,8 @@ class Solution:
         if not root: return True
         return self.checkSymmetric(root.left, root.right)
 
-    def checkSymmetric(self, node1, node2):
-        if not node1 and not node2: return True
-        if (not node1 and node2) or (not node2 and node1): return False
-        if node1.val != node2.val: return False
-        return self.checkSymmetric(node1.left, node2.right) and self.checkSymmetric(node1.right, node2.left)
-
     def isSymmetric_2(self, root: TreeNode) -> bool: # Iterative
         if not root: return True
-
-        def compare(arr):
-            start, end = 0, len(arr) - 1
-            while start < end:
-                if arr[start] != arr[end]: return False
-                start += 1
-                end -= 1
-            return True
-
         stack = [root]
         while len(stack):
             temp = []
@@ -39,8 +24,22 @@ class Solution:
                 if top.right: temp.append(top.right)
                 comp.append(top.left and top.left.val)
                 comp.append(top.right and top.right.val)
-            if not compare(comp): return False
+            if not self.compare(comp): return False
             stack = temp
+        return True
+
+    def checkSymmetric(self, node1, node2):
+        if not node1 and not node2: return True
+        if (not node1 and node2) or (not node2 and node1): return False
+        if node1.val != node2.val: return False
+        return self.checkSymmetric(node1.left, node2.right) and self.checkSymmetric(node1.right, node2.left)
+
+    def compare(self, node_list):
+        start, end = 0, len(node_list) - 1
+        while start < end:
+            if node_list[start] != node_list[end]: return False
+            start += 1
+            end -= 1
         return True
 
 
